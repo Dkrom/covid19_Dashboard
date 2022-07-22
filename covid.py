95b7c958b059b341a654dab7c3e58455d43ba5e4
Dkrom <86722467+Dkrom@users.noreply.github.com>
2022-07-22 22:20:45 +0530

    Create covid.py

---
+++ covid.py
@@ -0,0 +1,50 @@
+import numpy as np
+#import seaborn as sns
+import matplotlib.pyplot as plt
+import datetime
+import pandas as pd
+
+#reading the data from CSV file
+df=pd.read_csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv")
+#print(df.info())
+
+
+date=df.date
+df['date']=pd.to_datetime(df['date'], errors='coerce')
+#print(df)
+
+# list of all countries available in dataframe
+country=df['location'].to_list()
+set_country=set()	# list of all countries
+for i in country:
+	set_country.add(i)
+#print(set_country)	
+
+# set our country as India
+country="India"
+include_india=df[df['location'].values==country]
+exclude_india=df[df['location'].values!=country]
+india_cov19=include_india
+#print(india_cov19)
+
+# visualize covid19 cases in india
+plt.plot(india_cov19.date, india_cov19.new_cases)
+plt.xlabel('Date')
+plt.ylabel('New cases')
+plt.title("Covid-19 New cases in India")
+plt.show()
+
+# visualize covid19 deaths in India
+plt.plot(india_cov19.date, india_cov19.new_deaths)
+plt.xlabel('Date')
+plt.ylabel('New deaths')
+plt.title("Covid-19 deaths in India")
+plt.show()
+
+# visualize covid19 positivity in india
+plt.plot(india_cov19.date, india_cov19.positive_rate)
+plt.xlabel('Date')
+plt.ylabel('Positivity rate')
+plt.title("Covid-19 Positivity in India")
+plt.show()
+
